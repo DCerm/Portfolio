7abd2e61338a69255287d062be36439e01fde058
DCerm <138310097+DCerm@users.noreply.github.com>
2025-04-16 14:25:03 +0100

    Update fonts.ts

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
 import { Playwrite_CU, Syne, } from 'next/font/google';
  
-export const hand = Playwrite_CU({ subsets: ['latin'], weight: ['200', '300', '400'] });
+export const hand = Playwrite_CU({ weight: ['200', '300', '400'] });
 export const syne = Syne({ subsets: ['latin'], weight: ['400', '500', '600', '700', '800', ] });
